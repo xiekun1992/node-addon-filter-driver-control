7e29a77aef40c43ae6003d359057267c1e38d0fa
xiekun <xiekun@f3dt.com>
2019-12-29 10:51:21 +0800

    fix function call mistake

--- src/wrapper/filter_driver_control_wrapper.cpp
+++ src/wrapper/filter_driver_control_wrapper.cpp
@@ -6,7 +6,7 @@ namespace FilterDriverControl {
     return Napi::Boolean::New(info.Env(), fdc_obj.open_device());
   }
   Napi::Value close_device(const Napi::CallbackInfo& info) {
-    return Napi::Boolean::New(info.Env(), fdc_obj.open_device());
+    return Napi::Boolean::New(info.Env(), fdc_obj.close_device());
   }
   Napi::Value lock(const Napi::CallbackInfo& info) {
     return Napi::Boolean::New(info.Env(), fdc_obj.lock());
